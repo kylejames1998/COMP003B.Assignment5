cc5c90e4189a71ca13128aa74bd517b91459f9c7
Kyle James <140095432+kylejames1998@users.noreply.github.com>
2024-04-23 09:19:46 -0700

    added XML support

--- COMP003B.Assignment5/Program.cs
+++ COMP003B.Assignment5/Program.cs
@@ -1,3 +1,5 @@
+// added XML support
+using Microsoft.AspNetCore.Mvc.Formatters;
 
 namespace COMP003B.Assignment5
 {
@@ -8,6 +10,12 @@ namespace COMP003B.Assignment5
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            // added XML support
+            builder.Services.AddControllers(options =>
+            {
+                options.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+                options.InputFormatters.Add(new XmlSerializerInputFormatter(options));
+            });
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
