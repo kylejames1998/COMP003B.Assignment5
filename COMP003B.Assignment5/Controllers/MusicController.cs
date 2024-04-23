29fb6b731bff60cbb4bb2b1f82f8c48e5c09196f
Kyle James <140095432+kylejames1998@users.noreply.github.com>
2024-04-23 08:46:57 -0700

    Add project files.

---
+++ COMP003B.Assignment5/Controllers/MusicController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace COMP003B.Assignment5.Controllers
+{
+    public class MusicController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ COMP003B.Assignment5/Models/Music.cs
@@ -0,0 +1,11 @@
+﻿namespace COMP003B.Assignment5.Models
+{
+    public class Music
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Genre { get; set; }
+        public int Year { get; set; }
+    }
+}
